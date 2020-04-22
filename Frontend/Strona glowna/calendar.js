4c7e6e27d648f16ecd0429b8ca5783e965f54839
Krzysztof Wieckowski <krzysztof.wieckowski@ericsson.com>
2020-04-22 16:27:25 +0200

    Kalendarz - widok miesiąca

--- Frontend/Strona glowna/calendar.js
+++ Frontend/Strona glowna/calendar.js
@@ -100,12 +100,27 @@ class CalendarWeek {
 		//Add header title
 		var headerTitle = "";
 		
-		if((this.selectedDay.getMonth() == this.weekFirstDay.getMonth()) && (this.selectedDay.getMonth() == this.weekLastDay.getMonth())){
-			headerTitle = month_list[this.selectedDay.getMonth()] + " " + (this.weekFirstDay.getDate()) + "-" + (this.weekLastDay.getDate());
+		
+		if(this.weekFirstDay.getFullYear() == this.weekLastDay.getFullYear()){
+		
+			if((this.selectedDay.getMonth() == this.weekFirstDay.getMonth()) && (this.selectedDay.getMonth() == this.weekLastDay.getMonth())){
+				headerTitle = month_list[this.selectedDay.getMonth()] + " " + (this.weekFirstDay.getDate()) + "-" + (this.weekLastDay.getDate()) + ", " + (this.selectedDay.getFullYear());
+			} else {
+
+				headerTitle = month_list[this.weekFirstDay.getMonth()] + " " + (this.weekFirstDay.getDate()) + " - " + month_list[this.weekLastDay.getMonth()] + " " + (this.weekLastDay.getDate()) + ", " + (this.selectedDay.getFullYear());
+
+			}
+			
 		} else {
+			
 
-			headerTitle = month_list[this.weekFirstDay.getMonth()] + " " + (this.weekFirstDay.getDate()) + " - " + month_list[this.weekLastDay.getMonth()] + " " + (this.weekLastDay.getDate());
 
+				headerTitle = month_list[this.weekFirstDay.getMonth()] + " " + (this.weekFirstDay.getDate()) + ", " + this.weekFirstDay.getFullYear() + " - " + month_list[this.weekLastDay.getMonth()] + " " + (this.weekLastDay.getDate()) + ", " + (this.weekLastDay.getFullYear());
+
+			
+			
+			
+			
 		}
 		
 		this.divCalendarHeaderTitle.innerHTML = headerTitle;
@@ -385,7 +400,7 @@ class CalendarDay {
 		//Add header title
 		var headerTitle = "";
 		
-		headerTitle = day_list[this.selectedDay.getDay()] + " - " + (this.selectedDay.getDate()) + " " + month_list[this.selectedDay.getMonth()];
+		headerTitle = day_list[this.selectedDay.getDay()] + " - " + (this.selectedDay.getDate()) + " " + month_list[this.selectedDay.getMonth()] + ", " + this.selectedDay.getFullYear();
 	
 		this.divCalendarHeaderTitle.innerHTML = headerTitle;
 		
@@ -547,6 +562,263 @@ class CalendarDay {
 
 
 
+class CalendarMonth {
+	
+	
+	constructor (choosenDay) {
+		
+		//current day
+		this.today = new Date();
+		
+		//day for which calendar will be generated
+		this.selectedDay = new Date(choosenDay);
+			
+		
+		//number of days in the month
+        this.daysInMonth = new Date(this.selectedDay.getFullYear(), this.selectedDay.getMonth() + 1, 0).getDate();
+		
+		
+		//beginning of the month
+		this.monthFirstDay = new Date(this.selectedDay.getFullYear(), this.selectedDay.getMonth(), 1, 12, 0, 0, 0);
+		
+			//begining of the week with first day of the month
+			this.monthFirstDayWeekBeginning = new Date((this.monthFirstDay.getTime()-(this.monthFirstDay.getDay())*24*60*60*1000));
+		
+		
+		//last day in the month
+		this.monthLastDay = new Date(this.selectedDay.getFullYear(), this.selectedDay.getMonth(), this.daysInMonth, 12, 0, 0, 0);
+		
+			//end of the week with last day of the month
+			this.monthLastDayWeekEnding = new Date((this.monthLastDay.getTime()+(7 - 1 - this.monthLastDay.getDay())*24*60*60*1000));
+		
+		
+		
+        this.divCalendarCnt = null;
+        this.divCalendarWorkspace = null;
+        this.divCalendarHeader = null;
+        this.divCalendarMainView = null;
+        
+        this.divCalendarHeaderTitle = null;
+        this.divCalendarHeaderDayList = null;
+		
+		        
+        
+	}
+	
+	
+	
+	//Function to add calendar days descriptions
+	addDaysDescription() {
+		
+		this.divCalendarHeaderTitle.setAttribute("id","divCalendarHeaderTitle");
+	
+		month_list = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+		day_list = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+		
+		//Add header title
+		var headerTitle = "";
+		
+
+		headerTitle = month_list[this.selectedDay.getMonth()] + " " + (this.selectedDay.getFullYear());
+
+		
+		this.divCalendarHeaderTitle.innerHTML = headerTitle;
+		
+		
+		//keeps information about present calendar view (day for which this view was generated) - usufull when switching calendar view https://www.sitepoint.com/how-why-use-html5-custom-data-attributes/
+		this.divCalendarHeaderTitle.setAttribute("data-identifier", "M" + this.checkDateCode(this.selectedDay));
+			
+
+		
+		//Add days description
+		var i=0;
+		var insertHTMLcode = "";
+		
+		for (i; i<=6; i++)
+		{
+			
+			
+			insertHTMLcode += '<div class="calendarHeaderDay3">' + day_list[i] +'</div>';
+			//insertHTMLcode += '<div class="calendarHeaderDay3">' + (this.selectedDay.getDate()) + ' ' + day_list[this.selectedDay.getDay()] +'</div>';
+			
+		}
+		
+		this.divCalendarHeaderDayList.innerHTML = insertHTMLcode;
+	
+	
+	
+	}
+	
+	
+	
+	checkDateCode(checkDate) {
+		
+		var result = "";
+			
+		result += checkDate.getFullYear();
+				
+		if (checkDate.getMonth() < 10) {
+				
+			result += "0" + checkDate.getMonth();
+		} else {
+			result += checkDate.getMonth();
+		}
+			
+			
+		if (checkDate.getDate() < 10) {
+				
+			result += "0" + checkDate.getDate();
+		} else {
+			result += checkDate.getDate();
+		}
+		
+		return result;
+	
+	}
+	
+	
+	
+	checkHourCode(checkHour) {
+		
+		var result = "";
+		var min = ""
+		var hr = ""
+		
+		
+		hr = checkHour * 4;
+				
+			
+		if (hr < 10) {
+				
+			result = "_0" + hr + "00";
+		} else {
+			result = "_" + hr + "00";
+		}
+		
+		return result;
+		
+	}
+
+	
+	//Function to add calendar days descriptions
+	addDayFields() {
+		
+		var i=0;
+		var j=0;
+		var stop=0;
+		var insertHTMLcode = "";	
+		var tempDay = new Date(this.monthFirstDayWeekBeginning);
+		
+		//codes for div id
+		var dateCode = "";
+		var hourCode = "";
+		
+			
+		
+		do {
+			
+			insertHTMLcode += '<div class="calendarDaysRow">';
+			
+			
+				for (i; i<=6; i++)
+				{
+					
+					//Prepare day code - to add id atrribute to each div
+					dateCode = this.checkDateCode(tempDay);
+					
+					
+					insertHTMLcode += '<div class="calendarDayTile" id="' + dateCode + '">';
+					
+						if(tempDay.getMonth() == this.selectedDay.getMonth()) {					
+	
+							insertHTMLcode += '<div class="calendarDayTileHeader">' + tempDay.getDate() + '</div>';
+							
+						} else {
+							
+							insertHTMLcode += '<div class="calendarDayTileHeader2">' + tempDay.getDate() + '</div>';
+							
+						}
+						
+						
+					
+						for (j=0; j<=5; j++)
+						{
+							
+							hourCode = this.checkHourCode(j);
+							insertHTMLcode += '<div class="calendarDayTileHour" onclick="selectedByUser(this)" id="' + dateCode + hourCode + '">' + hourCode +'</div>';
+					
+						}
+						
+					
+					insertHTMLcode += '</div>';
+					
+					tempDay.setDate(tempDay.getDate()+1);
+			
+					
+					
+				}
+			
+				i = 0;
+			
+			
+			stop++;
+			
+			insertHTMLcode += '</div>';
+		}
+		while ((this.monthLastDayWeekEnding) > (tempDay) && stop<10); 
+		
+					
+		
+		this.divCalendarMainView.innerHTML = insertHTMLcode;
+		
+	}
+	
+	
+	
+	
+	init() {
+		
+		this.divCalendarCnt = document.createElement("div");
+		this.divCalendarCnt.classList.add("calendarCnt");
+		
+		this.divCalendarWorkspace = document.createElement("div");
+		this.divCalendarWorkspace.classList.add("calendarWorkSpace");
+		
+		this.divCalendarHeader = document.createElement("div");
+		this.divCalendarHeader.classList.add("calendarHeader");
+		
+		this.divCalendarMainView = document.createElement("div");
+		this.divCalendarMainView.classList.add("calendarMainView2");
+	
+		this.divCalendarHeaderTitle = document.createElement("div");
+		this.divCalendarHeaderTitle.classList.add("calendarHeaderTitle");
+		
+		this.divCalendarHeaderDayList = document.createElement("div");
+		this.divCalendarHeaderDayList.classList.add("calendarHeaderDayList");
+		
+		this.divCalendarCnt.appendChild(this.divCalendarWorkspace);
+		this.divCalendarWorkspace.appendChild(this.divCalendarHeader);
+		this.divCalendarWorkspace.appendChild(this.divCalendarMainView);
+		this.divCalendarHeader.appendChild(this.divCalendarHeaderTitle);
+		this.divCalendarHeader.appendChild(this.divCalendarHeaderDayList);
+		
+				
+		this.addDaysDescription();
+		this.addDayFields(); 
+		
+		
+		//------------------------
+		//this.divCalendarHeaderTitle.innerHTML = "April 10-17" + " week beginning " + this.weekFirstDay.getDate() + " " + this.weekFirstDay.getMonth() 
+		
+		//+ " The end of the week " + this.weekLastDay.getDate() + " " + this.weekLastDay.getMonth();
+		
+		
+	}
+	
+	
+
+	
+}
 
 
 
@@ -628,6 +900,12 @@ function generateCalendarView(dateSelected, viewType) {
 		document.getElementById("calendarViewWrapper").appendChild(cal.divCalendarCnt);
 		
 		addCalendarEvents(cal);
+	} else if (viewType == "M") {
+		const cal = new CalendarMonth(dateSelected);
+		cal.init();
+		document.getElementById("calendarViewWrapper").appendChild(cal.divCalendarCnt);
+		
+		addCalendarEvents(cal);
 	} else {
 		str1 = "sdhhsds";
 	}
@@ -659,7 +937,12 @@ function switchCalendar(options) {
 	} else if (calendarType == "D") {
 		jumpToDate = 1;
 	} else {
-		jumpToDate = 30;
+		
+		//number of days in a month
+		var numberOfDays = new Date(dayYear, dayMonth + 1, 0).getDate();
+		
+		
+		jumpToDate = numberOfDays;
 	}
 	
 	switch (options) {
@@ -677,7 +960,9 @@ function switchCalendar(options) {
 	case 3:
 		generateCalendarView(currentDay, "W");	
 		break;
-		
+	case 4:
+		generateCalendarView(currentDay, "M");	
+		break;	
 
 	
 	case 5:
@@ -701,7 +986,7 @@ function switchCalendar(options) {
 	
 	
 	
-	document.getElementById("footer").innerHTML = currentView + " " + calendarType + " " + dayYear + " " + dayMonth + " " + dayDate + " opton " + options + " jump " + jumpToDate;
+	//document.getElementById("footer").innerHTML = currentView + " " + calendarType + " " + dayYear + " " + dayMonth + " " + dayDate + " opton " + options + " jump " + jumpToDate;
 }
 	
 
