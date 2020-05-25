fe63befadd7bb0b371899b3e1e5f5d54496a9091
unknown <kw577@wp.pl>
2020-05-25 10:31:05 +0200

    Przeniesienie definicji beana mailSender do folderu src - w celu łatwiejszej konfiguracji spring-security

---
+++ familyOrganizer/src/main/java/proj/kw/familyOrganizer/backend/mailSending/EmailConfig.java
@@ -0,0 +1,31 @@
+package proj.kw.familyOrganizer.backend.mailSending;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+ 
+@Configuration
+public class EmailConfig 
+{
+    @Bean("mailSender")
+    public JavaMailSender getJavaMailSender() 
+    {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+          
+        mailSender.setUsername("mail@gmail.com");
+        mailSender.setPassword("password");
+          
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+          
+        return mailSender;
+    }
+     
+
+}
